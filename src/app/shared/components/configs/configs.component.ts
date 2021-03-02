6d76e5363cbd1bb3827b1c48df6f6734f84dac33
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-03-02 19:45:01 -0300

    idioma - localstorage

--- src/app/shared/components/configs/configs.component.ts
+++ src/app/shared/components/configs/configs.component.ts
@@ -11,12 +11,20 @@ export class ConfigsComponent implements OnInit {
   constructor(public translate: TranslateService) {
       translate.addLangs(['pt-br', 'en']);
       translate.setDefaultLang('pt-br');
-
-      const browserLang = translate.getBrowserLang();
-      translate.use(browserLang.match(/pt-br|en/) ? browserLang : 'pt-br');
+      this.setLanguage(this.getLanguage());
   }
 
   ngOnInit() {}
+
+  getLanguage(): string {
+    const lang = window.localStorage.getItem('lang');
+    return lang == null ? 'pt-br' : lang;
+  }
+
+  setLanguage(lang: string) {
+    window.localStorage.setItem('lang', lang);
+    this.translate.use(lang);
+  }
 
   toggle() {
     window.document.body.classList.toggle('dark');
