69822faf526a20a1d30abaea341d15dc77f008b6
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-03-03 11:11:37 -0300

    repositorios em grid responsiva

--- src/app/shared/components/list-repos/list-repos.component.ts
+++ src/app/shared/components/list-repos/list-repos.component.ts
@@ -8,28 +8,9 @@ import { Component, OnInit, VERSION } from '@angular/core';
   styleUrls: ['./list-repos.component.scss']
 })
 export class ListReposComponent implements OnInit {
-  ngVersion: string = VERSION.full;
-  matVersion: string = '5.1.0';
-  breakpoint: number = 0;
 
   constructor(public userService: UserService, public reposService: ReposService) { }
 
-  ngOnInit(): void {
-    this.setBreakpoint();
-  }
-
-  onResize(event: any) {
-    this.setBreakpoint();
-  }
-
-  setBreakpoint() {
-    if(window.innerWidth <= 650) {
-      this.breakpoint = 1;
-    }else if (window.innerWidth <= 950) {
-      this.breakpoint = 2;
-    }else {
-      this.breakpoint = 3;
-    }
-  }
+  ngOnInit(): void {}
 
 }
