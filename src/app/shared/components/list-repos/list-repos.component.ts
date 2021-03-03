ee9e56328ede1a46429f19d85ebde3145bcb042f
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-03-03 11:30:45 -0300

    display flex - lista repositorios menor que 650px

--- src/app/shared/components/list-repos/list-repos.component.ts
+++ src/app/shared/components/list-repos/list-repos.component.ts
@@ -5,12 +5,15 @@ import { Component, OnInit, VERSION } from '@angular/core';
 @Component({
   selector: 'app-list-repos',
   templateUrl: './list-repos.component.html',
-  styleUrls: ['./list-repos.component.scss']
+  styleUrls: ['./list-repos.component.scss'],
 })
 export class ListReposComponent implements OnInit {
+  itemsPerPage: number = 6;
 
-  constructor(public userService: UserService, public reposService: ReposService) { }
+  constructor(
+    public userService: UserService,
+    public reposService: ReposService
+  ) {}
 
   ngOnInit(): void {}
-
 }
