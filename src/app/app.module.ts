f797c3b6e905e933f57dd9d1f18c3318117775f1
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-03-04 23:35:04 -0300

    notificacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +19,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
 
-import { HomeUserComponent, QrcodeDialog } from './core/components/home-user/home-user.component';
+import {
+  HomeUserComponent,
+  QrcodeDialog,
+} from './core/components/home-user/home-user.component';
 import { HomeNoUserComponent } from './core/components/home-no-user/home-no-user.component';
 import { ButtonComponent } from './shared/components/button/button.component';
 import { ButtonBackComponent } from './shared/components/button-back/button-back.component';
@@ -29,6 +32,7 @@ import { GithubCornerComponent } from './shared/components/github-corner/github-
 import { QrcodeComponent } from './shared/components/qrcode/qrcode.component';
 import { ConfigsComponent } from './shared/components/configs/configs.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { SwPush, SwUpdate } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
@@ -47,7 +51,7 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
     GithubCornerComponent,
     QrcodeComponent,
     QrcodeDialog,
-    ConfigsComponent
+    ConfigsComponent,
   ],
   imports: [
     BrowserModule,
@@ -69,12 +73,41 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
+        deps: [HttpClient],
+      },
     }),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  VAPID_PUBLIC_KEY =
+    'BKuvEyR0kNzgO0zrjXH9I666UQzpFVDfwgdnOMyqrs3TLJIAeyDtTyM6Qjvf9KU-QvAjoCPmgAfzDpdxugsqbF0';
+  constructor(private pushSw: SwPush, private update: SwUpdate) {
+    update.available.subscribe((update) => {
+      console.log('Nova versão disponível');
+    });
+
+    this.SubscribeToPush();
+    pushSw.messages.subscribe((msg) => {
+      console.log(JSON.stringify(msg));
+    });
+  }
+
+  SubscribeToPush() {
+    this.pushSw
+      .requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY,
+      })
+      .then((pushSubscription) => {
+        console.log(JSON.stringify(pushSubscription));
+      })
+
+      .catch((err) => {
+        console.error('Ocorreu um erro:' + err);
+      });
+  }
+}
