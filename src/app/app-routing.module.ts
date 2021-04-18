7af1139a49b6c95f144e84fbebf3d026906cb09b
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-04-18 12:29:32 -0300

    add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { HomeComponent } from './shared/components/home/home.component';
+import { ListFollowingComponent } from './shared/components/list-following/list-following.component';
+import { ListFollowersComponent } from './shared/components/list-followers/list-followers.component';
 import { HomeUserComponent } from './core/components/home-user/home-user.component';
 import { HomeNoUserComponent } from './core/components/home-no-user/home-no-user.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'user/:login', component: HomeUserComponent },
+  { path: 'user/:login', component: HomeUserComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'followers', component: ListFollowersComponent },
+      { path: 'following', component: ListFollowingComponent }
+    ]
+  },
   { path: 'user', component: HomeNoUserComponent },
   { path: '', component: HomeNoUserComponent }
 ];
