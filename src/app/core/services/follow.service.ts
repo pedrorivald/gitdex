a457e2c143a388ec9f3bef33f8d9824afbdddf24
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-04-18 16:49:33 -0300

    fix: conditional for page

--- src/app/core/services/follow.service.ts
+++ src/app/core/services/follow.service.ts
@@ -11,14 +11,20 @@ export class FollowService {
   listFollowers: Follow[] = [];
   listFollowing: Follow[] = [];
   loading: boolean = false;
-  public currentPage = 1;
+  pageFollowers = 0;
+  pageFollowing = 0;
 
   constructor(private userService: UserService) { }
 
   getFollowersPerPage() {
     let page = Math.ceil(this.listFollowers.length / this.followPerPage) + 1;
+
+    if(this.pageFollowers != page) {
+      this.pageFollowers = page;
+    } else { return; }
+
     this.loading = true;
-    this.userService.getFollowersPerPage(this.userService.user.login, page, this.followPerPage)
+    this.userService.getFollowersPerPage(this.userService.user.login, this.pageFollowers, this.followPerPage)
       .subscribe((follow) => {
         this.listFollowers = this.listFollowers.concat(follow);
         this.loading = false;
@@ -27,8 +33,13 @@ export class FollowService {
 
   getFollowingPerPage() {
     let page = Math.ceil(this.listFollowing.length / this.followPerPage) + 1;
+
+    if(this.pageFollowing != page) {
+      this.pageFollowing = page;
+    } else { return; }
+
     this.loading = true;
-    this.userService.getFollowingPerPage(this.userService.user.login, page, this.followPerPage)
+    this.userService.getFollowingPerPage(this.userService.user.login, this.pageFollowing, this.followPerPage)
       .subscribe((follow) => {
         this.listFollowing = this.listFollowing.concat(follow);
         this.loading = false;
