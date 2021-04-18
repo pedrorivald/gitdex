d658e7e289dad3de3f5624eb310a4e16f7b13bfd
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-04-18 16:48:12 -0300

    feat: button back and reset

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,3 +1,5 @@
+import { ReposService } from './../../../core/services/repos.service';
+import { NavigateService } from './../../../core/services/navigate.service';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/core/services/user.service';
 import { VoicesService } from 'src/app/core/services/voices.service';
@@ -5,12 +7,21 @@ import { VoicesService } from 'src/app/core/services/voices.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(public userService: UserService, public voicesService: VoicesService) { }
+  constructor(
+    public userService: UserService,
+    public voicesService: VoicesService,
+    public navigateService: NavigateService,
+    private reposService: ReposService
+  ) {}
 
   ngOnInit(): void {}
 
+  reset() {
+    this.userService.reset();
+    this.reposService.reset();
+    this.voicesService.reset();
+  }
 }
