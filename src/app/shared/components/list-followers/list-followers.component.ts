a0c5ef810bb682c9ead251b5edd791aaba674efb
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-04-18 16:37:28 -0300

    feat: button back and reset

--- src/app/shared/components/list-followers/list-followers.component.ts
+++ src/app/shared/components/list-followers/list-followers.component.ts
@@ -1,22 +1,34 @@
+import { UserService } from 'src/app/core/services/user.service';
+import { NavigateService } from './../../../core/services/navigate.service';
 import { Component, OnInit } from '@angular/core';
 import { FollowService } from 'src/app/core/services/follow.service';
 
 @Component({
   selector: 'app-list-followers',
   templateUrl: './list-followers.component.html',
-  styleUrls: ['./list-followers.component.scss']
+  styleUrls: ['./list-followers.component.scss'],
 })
 export class ListFollowersComponent implements OnInit {
-
   throttle = 50;
   scrollDistance = 1;
 
-  constructor( public followService: FollowService) {}
+  constructor(
+    public followService: FollowService,
+    public navigateService: NavigateService,
+    public userService: UserService
+  ) {}
 
-  ngOnInit(): void { this.followService.getFollowersPerPage(); }
+  ngOnInit(): void {
+    this.reset();
+    this.followService.getFollowersPerPage();
+  }
+
+  reset() {
+    this.followService.listFollowers = [];
+    this.followService.pageFollowers = 0;
+  }
 
   onScrollDown() {
     this.followService.getFollowersPerPage();
   }
-
 }
--- src/app/shared/components/list-following/list-following.component.ts
+++ src/app/shared/components/list-following/list-following.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FollowService } from 'src/app/core/services/follow.service';
+import { NavigateService } from 'src/app/core/services/navigate.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-list-following',
@@ -11,9 +13,21 @@ export class ListFollowingComponent implements OnInit {
   throttle = 50;
   scrollDistance = 1;
 
-  constructor( public followService: FollowService) {}
+  constructor(
+    public followService: FollowService,
+    public navigateService: NavigateService,
+    public userService: UserService
+  ) {}
 
-  ngOnInit(): void { this.followService.getFollowingPerPage(); }
+  ngOnInit(): void {
+    this.reset();
+    this.followService.getFollowingPerPage();
+  }
+
+  reset() {
+    this.followService.listFollowing = [];
+    this.followService.pageFollowing = 0;
+  }
 
   onScrollDown() {
     this.followService.getFollowingPerPage();
