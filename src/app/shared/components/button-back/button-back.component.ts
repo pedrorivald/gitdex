be85761dcfd13e9877b3ac14dc4fca3ce1273761
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-04-24 19:45:52 -0300

    fix: color button back

--- src/app/shared/components/button-back/button-back.component.ts
+++ src/app/shared/components/button-back/button-back.component.ts
@@ -9,7 +9,20 @@ export class ButtonBackComponent implements OnInit {
 
   constructor() { }
 
+  changeColor = false;
+
   ngOnInit(): void {
+    window.onscroll = () => this.withUserOnTop();
+  }
+
+  withUserOnTop() {
+    let toTop = document.getElementById('with-user')?.getBoundingClientRect().top || 100;
+    this.changeColor = (this.getDarkmode() == 'false' && toTop < 10);
+  }
+
+  getDarkmode(): string {
+    const darkmode = window.localStorage.getItem('darkmode');
+    return darkmode == null ? '' : darkmode;
   }
 
 }
