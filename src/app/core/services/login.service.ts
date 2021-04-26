972acaf50801e2256cfa4e8017d3709197e8f0b7
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-04-26 20:56:19 -0300

    feat: error message - user not found

--- src/app/core/services/login.service.ts
+++ src/app/core/services/login.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
 import { NavigateService } from './navigate.service';
 import { environment } from 'src/environments/environment';
 import { user } from 'src/app/shared/models/user';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { TranslatorService } from './translator.service';
 
 @Injectable({
   providedIn: 'root',
@@ -14,8 +16,13 @@ export class LoginService {
     public userService: UserService,
     public starredService: StarredService,
     private githubService: GithubService,
-    private navigateService: NavigateService
+    private navigateService: NavigateService,
+    private _snackBar: MatSnackBar,
+    private translator: TranslatorService,
   ) {}
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
   login(login: string) {
     this.userService.loading = true;
@@ -30,6 +37,15 @@ export class LoginService {
         this.starredService.pageTotalStarred = 0;
         this.starredService.getStarred();
       }
+    }, (error) => {
+      this.userService.loading = false;
+      this._snackBar.open(this.translator.get('SNACKBAR.USER-NOT-FOUND'), 'X', {
+        duration: 1500,
+        horizontalPosition: this.horizontalPosition,
+        verticalPosition: this.verticalPosition,
+        panelClass: ['custom-class'],
+      });
+      this.navigateService.navigateToHomeNoUser();
     });
   }
 
