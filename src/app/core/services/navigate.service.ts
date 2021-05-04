ce7cf34e56415374e6ed622077a9c81136b1ed4f
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-05-04 19:12:17 -0300

    feat: reset title

--- src/app/core/services/navigate.service.ts
+++ src/app/core/services/navigate.service.ts
@@ -12,6 +12,7 @@ export class NavigateService {
   }
 
   navigateToHomeNoUser() {
+    window.document.getElementsByTagName('title')[0].innerHTML = `GitDex`;
     this.router.navigate(['']);
   }
 }
